1ea2dd624b7e0aaa0dc2a1543a23509daa7b231d
angelestrada14019 <rangel1998.rt@gmail.com>
2022-04-24 12:07:49 -0500

    agregando swagger al proyectyo

--- src/main/java/com/hospitalajea14019/projecthospitalspring/config/SecurityConfig.java
+++ src/main/java/com/hospitalajea14019/projecthospitalspring/config/SecurityConfig.java
@@ -38,6 +38,21 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter { //redefinir a
         return new BCryptPasswordEncoder();
     }
 
+    private static final String[] AUTH_WHITELIST = {
+            // -- Swagger UI v2
+            "/v2/api-docs",
+            "/swagger-resources",
+            "/swagger-resources/**",
+            "/configuration/ui",
+            "/configuration/security",
+            "/swagger-ui.html",
+            "/webjars/**",
+            // -- Swagger UI v3 (OpenAPI)
+            "/v3/api-docs/**",
+            "/swagger-ui/**"
+            // other public endpoints of your API may be appended to this array
+    };
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.cors().and()
@@ -55,6 +70,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter { //redefinir a
                 .antMatchers("/paciente/**").hasAnyRole("ADMIN","PACI")
                 .antMatchers("/domicilio/**").hasAnyRole("ADMIN","PACI")
                 .antMatchers("/turno/**").hasRole("ADMIN")
+                .antMatchers(AUTH_WHITELIST).permitAll()
                 .anyRequest()
                 .authenticated();
         http.addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class); //autenticacion por token (filtro usado y la clase del filtro
